ea703b3ae5380b4ea134f02192b5d89bec054826
Konstantin Menovschikov <free-wap@mail.ru>
2026-01-06 13:59:48 +0300

    Minus sss

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-ssss
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
